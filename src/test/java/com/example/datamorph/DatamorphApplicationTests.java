ec0674ac01e38b87228b172288e0a8fdda97ecc7
berazo29 <37014465+berazo29@users.noreply.github.com>
2023-10-08 20:33:08 -0400

    test: add missing filename arg

--- src/test/java/com/example/datamorph/DatamorphApplicationTests.java
+++ src/test/java/com/example/datamorph/DatamorphApplicationTests.java
@@ -3,9 +3,8 @@ package com.example.datamorph;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(args = {"flights.csv"})
 class DatamorphApplicationTests {
-
     @Test
     void contextLoads() {
     }
