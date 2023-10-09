b0fae2375cfd6b0eccce531951b27fcce01c2188
berazo29 <37014465+berazo29@users.noreply.github.com>
2023-10-08 23:26:11 -0400

    feat: add default and yarn spark session config

--- src/main/java/com/example/datamorph/ApplicationConfig.java
+++ src/main/java/com/example/datamorph/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.example.datamorph;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.SparkSession;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +10,16 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 public class ApplicationConfig {
     @Bean
-    public SparkSession sparkSession() {
-        log.info("Starting local Spark Session");
-        return SparkSession.builder().master("local[4]").appName("APP SPARK").getOrCreate();
+    @ConditionalOnProperty(name = "service.type", havingValue = "default")
+    public SparkSession sparkSessionLocal() {
+        log.info("Starting default Spark Session.");
+        return SparkSession.builder().master("local").appName("APP SPARK").getOrCreate();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "service.type", havingValue = "yarn")
+    public SparkSession sparkSessionYarn() {
+        log.info("Starting Spark Session with Yarn.");
+        return SparkSession.builder().master("yarn").appName("APP SPARK").getOrCreate();
     }
 }
